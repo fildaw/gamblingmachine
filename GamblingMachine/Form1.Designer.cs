611b7844b26b59e2fb72aafa9ec1733befdc1759
Filip Dawidowski <58572075+fildaw@users.noreply.github.com>
2021-02-13 11:35:03 +0100

    Add files via upload

---
+++ GamblingMachine/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace GamblingMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.box3 = new System.Windows.Forms.PictureBox();
+            this.box2 = new System.Windows.Forms.PictureBox();
+            this.box1 = new System.Windows.Forms.PictureBox();
+            this.machine = new System.Windows.Forms.PictureBox();
+            this.current_points = new System.Windows.Forms.Label();
+            this.added_points = new System.Windows.Forms.Label();
+            this.message = new System.Windows.Forms.Label();
+            this.Info = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.oGrzeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Instruction = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.box3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.machine)).BeginInit();
+            this.Info.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // box3
+            // 
+            this.box3.Image = global::GamblingMachine.Properties.Resources.seven;
+            this.box3.Location = new System.Drawing.Point(552, 229);
+            this.box3.Name = "box3";
+            this.box3.Size = new System.Drawing.Size(107, 106);
+            this.box3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.box3.TabIndex = 5;
+            this.box3.TabStop = false;
+            // 
+            // box2
+            // 
+            this.box2.Image = global::GamblingMachine.Properties.Resources.seven;
+            this.box2.Location = new System.Drawing.Point(391, 229);
+            this.box2.Name = "box2";
+            this.box2.Size = new System.Drawing.Size(107, 106);
+            this.box2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.box2.TabIndex = 4;
+            this.box2.TabStop = false;
+            // 
+            // box1
+            // 
+            this.box1.Image = global::GamblingMachine.Properties.Resources.seven;
+            this.box1.Location = new System.Drawing.Point(235, 229);
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(107, 106);
+            this.box1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.box1.TabIndex = 1;
+            this.box1.TabStop = false;
+            // 
+            // machine
+            // 
+            this.machine.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.machine.Image = global::GamblingMachine.Properties.Resources.gambling_bg;
+            this.machine.Location = new System.Drawing.Point(0, 0);
+            this.machine.Name = "machine";
+            this.machine.Size = new System.Drawing.Size(895, 546);
+            this.machine.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.machine.TabIndex = 0;
+            this.machine.TabStop = false;
+            this.Instruction.SetToolTip(this.machine, "Kliknij, aby zagrać");
+            this.machine.MouseDown += new System.Windows.Forms.MouseEventHandler(this.machine_Click);
+            // 
+            // current_points
+            // 
+            this.current_points.AutoSize = true;
+            this.current_points.BackColor = System.Drawing.Color.White;
+            this.current_points.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.current_points.ForeColor = System.Drawing.Color.White;
+            this.current_points.Location = new System.Drawing.Point(583, 12);
+            this.current_points.Name = "current_points";
+            this.current_points.Size = new System.Drawing.Size(76, 26);
+            this.current_points.TabIndex = 6;
+            this.current_points.Text = "label1";
+            // 
+            // added_points
+            // 
+            this.added_points.AutoSize = true;
+            this.added_points.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.added_points.Location = new System.Drawing.Point(642, 22);
+            this.added_points.Name = "added_points";
+            this.added_points.Size = new System.Drawing.Size(46, 17);
+            this.added_points.TabIndex = 7;
+            this.added_points.Text = "label1";
+            this.added_points.Visible = false;
+            // 
+            // message
+            // 
+            this.message.BackColor = System.Drawing.Color.White;
+            this.message.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.message.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.message.ForeColor = System.Drawing.Color.White;
+            this.message.Location = new System.Drawing.Point(0, 515);
+            this.message.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(895, 31);
+            this.message.TabIndex = 8;
+            this.message.Text = "label1";
+            this.message.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Info
+            // 
+            this.Info.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oGrzeToolStripMenuItem});
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(105, 26);
+            // 
+            // oGrzeToolStripMenuItem
+            // 
+            this.oGrzeToolStripMenuItem.Name = "oGrzeToolStripMenuItem";
+            this.oGrzeToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.oGrzeToolStripMenuItem.Text = "Autor";
+            this.oGrzeToolStripMenuItem.Click += new System.EventHandler(this.oGrzeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(895, 546);
+            this.ContextMenuStrip = this.Info;
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.added_points);
+            this.Controls.Add(this.current_points);
+            this.Controls.Add(this.box3);
+            this.Controls.Add(this.box2);
+            this.Controls.Add(this.box1);
+            this.Controls.Add(this.machine);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Midnight Casino";
+            ((System.ComponentModel.ISupportInitialize)(this.box3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.machine)).EndInit();
+            this.Info.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox machine;
+        private System.Windows.Forms.PictureBox box1;
+        private System.Windows.Forms.PictureBox box2;
+        private System.Windows.Forms.PictureBox box3;
+        private System.Windows.Forms.Label current_points;
+        private System.Windows.Forms.Label added_points;
+        private System.Windows.Forms.Label message;
+        private System.Windows.Forms.ContextMenuStrip Info;
+        private System.Windows.Forms.ToolStripMenuItem oGrzeToolStripMenuItem;
+        private System.Windows.Forms.ToolTip Instruction;
+    }
+}
+
---
+++ GamblingMachine/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Media;
+using System.IO;
+using System.Reflection;
+using System.Windows.Media;
+
+//Cherry: +10; Grapes: +10; Apple: +10; Seven: +30
+//Wszystkie ikonki inne (lub dwa nie obok siebie): -45
+//Dwie ikonki te same obok siebie: -30
+//Wszystkie ikonki te same: -0
+
+namespace GamblingMachine
+{
+    public partial class Form1 : Form
+    {
+        static Image seven = new Bitmap(GamblingMachine.Properties.Resources.seven);
+
+        static Image apple = new Bitmap(GamblingMachine.Properties.Resources.apple);
+        static Image cherries = new Bitmap(GamblingMachine.Properties.Resources.cherries);
+        static Image grapes = new Bitmap(GamblingMachine.Properties.Resources.grapes);
+
+        static Image[] normal = new Image[] { apple, cherries, grapes };
+
+        static Image empty = new Bitmap(GamblingMachine.Properties.Resources.empty_box);
+
+        static Image machine_down = new Bitmap(GamblingMachine.Properties.Resources.gambling_down_bg);
+        static Image machine_normal = new Bitmap(GamblingMachine.Properties.Resources.gambling_bg);
+        static Image machine_motion = new Bitmap(GamblingMachine.Properties.Resources.gambling_motion_bg);
+
+        Boolean wait = false;
+        Boolean permanentBlock = false;
+
+        string[] minusPoints = new string[] { "Następnym razem będzie lepiej!", "Nie poddawaj się!", "Trening czyni mistrza!" };
+        string[] level1 = new string[] { "Nieźle!", "Świetnie!", "Nabierasz wprawy!" }; // > 0
+        string[] level2 = new string[] { "Właśnie tak!", "Tak trzymaj!", "Wielka fortuna jest tuż za rogiem!" }; // > 15
+        string[] level3 = new string[] { "MASTER!", "Mistrz!", "Niebywały szcześciarz!" }; //> 30
+        string[] level4 = new string[] { "TRIPLE SEVEN", "LEGENDA!!!!" }; // > 80
+
+        int currPoints = 100;
+
+        MediaPlayer sound = new MediaPlayer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            current_points.Text = currPoints.ToString();
+            added_points.Visible = false;
+            message.Visible = false;
+            current_points.BackColor = System.Drawing.Color.FromArgb(91, 68, 53);
+            added_points.BackColor = System.Drawing.Color.FromArgb(91, 68, 53);
+            message.BackColor = System.Drawing.Color.FromArgb(108, 87, 76);
+            Stream resource = Assembly.GetExecutingAssembly().GetManifestResourceStream("GamblingMachine.Resources.Machine_working.wav");
+            Stream output = File.Create(Path.GetTempPath() + "Machine_working.wav");
+            try
+            {
+                resource.CopyTo(output);
+            } catch (Exception e)
+            {
+                MessageBox.Show("Brak dostępu do folderu z plikami tymczasowymi!");
+            }
+            
+            this.Icon = Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
+            resource.Close();
+            output.Close();
+        }
+
+        void OnProcessExit(object sender, EventArgs e)
+        {
+            try
+            {
+                File.Delete(Path.Combine(Path.GetTempPath(), "Machine_working.wav"));
+            } catch (Exception ex)
+            {
+                MessageBox.Show("Nie można usunąć plików tymczasowych!");
+            }
+        }
+
+        private void drawBox(PictureBox box)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(1, 101);
+            Debug.WriteLine(num);
+
+            if (num > 0 && num <= 10) //10%
+            {
+                box.Image = seven;
+            } else if (num > 10 && num <= 40) //30%
+            {
+                box.Image = apple;
+            } else if (num > 40 && num <= 70) //30%
+            {
+                box.Image = cherries;
+            } else if (num > 70 && num <= 100) //30%
+            {
+                box.Image = grapes;
+            }
+            Stream str = Properties.Resources.Fruit_placed;
+            SoundPlayer simpleSound = new SoundPlayer(str);
+            simpleSound.Play();
+        }
+
+        private void resetBoxes()
+        {
+            box1.Image = empty;
+            box2.Image = empty;
+            box3.Image = empty;
+        }
+
+        private int calculatePoints(PictureBox[] boxes)
+        {
+            int points = 0;
+            foreach (PictureBox box in boxes)
+            {
+                if (normal.Contains(box.Image))
+                {
+                    points += 10;
+                } else if (box.Image == seven)
+                {
+                    points += 30;
+                }
+            }
+
+            if (boxes[0].Image == boxes[1].Image && boxes[1].Image == boxes[2].Image)
+            {
+                //Skip
+            } else if (boxes[0].Image == boxes[1].Image || boxes[1].Image == boxes[2].Image) //Tylko dwa obok siebie
+            {
+                points -= 30;
+            } else
+            {
+                points -= 45;
+            }
+
+            return points;
+        }
+
+        private void displayMessage(int receivedPoints)
+        {
+            Random rnd = new Random();
+            if (receivedPoints <= 0)
+            {
+                int i = rnd.Next(0, minusPoints.Length);
+                message.Text = minusPoints[i];
+            } else if (receivedPoints > 0 && receivedPoints <= 15)
+            {
+                int i = rnd.Next(0, level1.Length);
+                message.Text = level1[i];
+            } else if (receivedPoints > 15 && receivedPoints <= 30)
+            {
+                int i = rnd.Next(0, level2.Length);
+                message.Text = level2[i];
+            } else if (receivedPoints > 30 && receivedPoints <= 80)
+            {
+                int i = rnd.Next(0, level3.Length);
+                message.Text = level3[i];
+            } else
+            {
+                int i = rnd.Next(0, level4.Length);
+                message.Text = level4[i];
+            }
+        }
+
+        private async void pointsAnimation(int basePoints, int endPoints)
+        {
+            Boolean add = true;
+            if (endPoints - basePoints == 0)
+            {
+                return;
+            }
+            if (endPoints < basePoints)
+            {
+                add = false;
+            }
+
+            int difference = Math.Abs(Math.Abs(endPoints) - Math.Abs(basePoints));
+            int tempPoints = basePoints;
+            int step = 1;
+            for (int i = 1; i <= difference; i++)
+            {
+                if (add)
+                {
+                    tempPoints += step;
+                } else
+                {
+                    tempPoints -= step;
+                }
+
+                current_points.Text = tempPoints.ToString();
+                await Task.Delay(50);
+            }
+            current_points.Text = endPoints.ToString();
+        }
+
+        private async void entryFee()
+        {
+
+            added_points.Visible = true;
+            added_points.Text = "-2";
+            added_points.ForeColor = System.Drawing.Color.Red;
+            pointsAnimation(currPoints, currPoints - 2);
+            currPoints -= 2;
+            await Task.Delay(1000);
+            added_points.Visible = false;
+        }
+
+        private async void machine_Click(object sender, MouseEventArgs e)
+        {
+            if (!wait && e.Button == MouseButtons.Left && !permanentBlock)
+            {
+                wait = true;
+                this.Cursor = Cursors.WaitCursor;
+                entryFee();
+                resetBoxes();
+
+                Stream str = Properties.Resources.Lever_down;
+                SoundPlayer simpleSound = new SoundPlayer(str);
+                simpleSound.Play();
+
+                machine.Image = machine_down;
+                await Task.Delay(500);
+                machine.Image = machine_motion;
+                await Task.Delay(400);
+
+                sound.Open(new Uri(Path.Combine(Path.GetTempPath(), "Machine_working.wav")));
+                sound.Position = TimeSpan.Zero;
+                sound.Play();
+
+                await Task.Delay(1500);
+                drawBox(box1);
+                await Task.Delay(1500);
+                drawBox(box2);
+                await Task.Delay(1500);
+                drawBox(box3);
+                machine.Image = machine_normal;
+
+                PictureBox[] boxes = new PictureBox[] { box1, box2, box3 };
+                int receivedPoints = calculatePoints(boxes);
+                if (receivedPoints < 0)
+                {
+                    added_points.ForeColor = System.Drawing.Color.Red;
+                    added_points.Text = "";
+                } else
+                {
+                    added_points.ForeColor = System.Drawing.Color.LightGreen;
+                    added_points.Text = "+";
+                }
+
+                added_points.Text += receivedPoints.ToString();
+                added_points.Visible = true;
+                pointsAnimation(currPoints, currPoints + receivedPoints);
+                currPoints += receivedPoints;
+
+                displayMessage(receivedPoints);
+                message.Visible = true;
+                
+                this.Cursor = Cursors.Default;
+                if (currPoints < 0)
+                {
+                    MessageBox.Show("Niestety zbankrutowałeś.");
+                    permanentBlock = true;
+                }
+                await Task.Delay(2000);
+                
+                message.Visible = false;
+                wait = false;
+            }
+            
+        }
+
+        private void oGrzeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("© 2021 Filip Dawidowski @ VLO Gdańsk");
+        }
+    }
+}
